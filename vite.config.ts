e0df3d02e58742d7e822b940817b0900b41dc943
lorem10 <momenei.alireza1382.15@gmail.com>
2025-12-08 03:14:25 +0330

    chore: setup vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs-extra'
+import matter from 'gray-matter'
 import UnoCSS from 'unocss/vite'
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue(), UnoCSS(), VueRouter({
+    routesFolder: 'pages',
+    extensions: ['.vue', '.md'],
+    extendRoute(route) {
+      const path = route.components.get('default')
+      if (!path)
+        return
+      const { data } = matter(fs.readFileSync(path, 'utf-8'))
+
+      route.addToMeta({
+        frontmatter: data,
+      })
+    },
+  })],
 })
