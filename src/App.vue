604e24d04f9756f0b4d470d97b0b29e8822464e7
lorem10 <momenei.alireza1382.15@gmail.com>
2025-12-08 05:14:22 +0330

    chore: adjust footer and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import Footer from './components/Footer.vue'
+</script>
+
 <template>
-  <main class="container mx-auto">
-    <RouterView />
-  </main>
+  <div class="p-sp-3 font-primary md:p-sp-5">
+    <main class="container mx-auto">
+      <RouterView />
+      <Footer />
+    </main>
+  </div>
 </template>
