a3c2cf05e8572e5386a2cfde0f2d4385858d4bf0
lorem10 <momenei.alireza1382.15@gmail.com>
2025-12-08 07:43:08 +0330

    feat: implement separator component

---
+++ src/components/Separator.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { type = 'dynamic' } = defineProps<{
+  type?: 'alone' | 'twin' | 'dynamic'
+}>()
+</script>
+
+<template>
+  <div
+    class="mx-auto my-sp-6 flex items-end justify-between text-2xl color-base-secondary lg:text-3xl"
+    :class="type !== 'twin' ? 'md:justify-center' : 'md:justify-between'"
+  >
+    <div class="i-ri-add-large-line" />
+    <div
+      class="i-ri-add-large-line"
+      :class="{ 'md:hidden': type !== 'twin' }"
+    />
+  </div>
+</template>
