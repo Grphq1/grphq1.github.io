e75e99239a88558acd9e44fc97c89f1051a0fc3f
lorem10 <momenei.alireza1382.15@gmail.com>
2025-12-08 05:14:09 +0330

    feat: implement footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="m-auto mt-sp-5 w-full flex justify-center text-center prose"
+  >
+    <span class="text-sm op50">
+      <a
+        target="_blank"
+        href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+        style="color: inherit"
+      >CC BY-NC-SA 4.0</a>
+      2025 © Alireza
+    </span>
+  </div>
+</template>
